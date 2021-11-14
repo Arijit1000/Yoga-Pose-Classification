57022ab46358c379f3d25ca92237c0cbb103846c
Arijit1000 <73710898+Arijit1000@users.noreply.github.com>
2021-11-14 21:26:31 +0530

    Update add_prefix.sh

--- src/add_prefix.sh
+++ src/add_prefix.sh
@@ -6,4 +6,4 @@ for file in *.png;do cp $file "$1_$file";done
 #for dir in `ls|fgrep -v zip`;do cd $dir;bash ~/proj/capstone/scripts/add_prefix.sh google;cd ..;done
 
 #hwo to move prefixed files to interim dir
-#for dir in `ls`;do mv ../youtube_yoga_frames/$dir/ $dir/.;done
+#for dir in `ls`;do mv ../youtube_yoga_frames/$dir/youtube* $dir/.;done
